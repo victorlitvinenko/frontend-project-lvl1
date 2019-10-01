803a87288f5a77dadbbeb2f29d2f454e9567dc02
Victor Litvinenko <vitosik@gmail.com>
2019-10-01 10:59:55 +0300

    Babel connection

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: {
+        node: 'current',
+        firefox: '60',
+        chrome: '67',
+        safari: '11.1',
+      },
+    }],
+  ],
+};
