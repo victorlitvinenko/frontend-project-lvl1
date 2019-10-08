f6a7cb7a69d6bde1a42772e38549c2425ca5ee7a
Victor Litvinenko <victor@inetlog.ru>
2019-10-08 21:51:40 +0300

    fix (brain-prime.js) fix isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,12 +6,10 @@ const PRIME_MAX = 100;
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getQuestionAndCorrectAnswer = () => {
   const isPrime = (num) => {
-    if (num < 2) return false;
-    let count = 0;
-    for (let i = 1; i <= num / 2; i += 1) {
-      if (num % i === 0) count += i;
+    for (let i = 2; i <= num / 2; i += 1) {
+      if (num % i === 0) return false;
     }
-    return count === num;
+    return num > 1;
   };
   const question = random(1, PRIME_MAX);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
