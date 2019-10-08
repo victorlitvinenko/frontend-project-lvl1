8ca51daea6770e1543221522815acf321c600e64
Victor Litvinenko <victor@inetlog.ru>
2019-10-08 22:30:04 +0300

    chore (brain-prime.js) replace isPrime outside

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,15 +2,16 @@ import { cons } from '@hexlet/pairs';
 import random from '../libs/random';
 import playGame from '../engine';
 
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
 const PRIME_MAX = 100;
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
 const getQuestionAndCorrectAnswer = () => {
-  const isPrime = (num) => {
-    for (let i = 2; i <= num / 2; i += 1) {
-      if (num % i === 0) return false;
-    }
-    return num > 1;
-  };
   const question = random(1, PRIME_MAX);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return cons(question, correctAnswer);
